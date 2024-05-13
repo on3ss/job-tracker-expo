f530dc18ab6e8ded2a9a245202bf010e653bfbf1
Oness Lyngdoh <auditoree9@gmail.com>
2024-05-13 13:01:23 +0530

    cleanup

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from './src/screens/HomeScreen';
 import AppBottomTab from './src/navigation/BottomTabBar';
 
 export default function App() {
