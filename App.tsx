bd7943d10f1aff7278d5e8a0b0dc42adb1027d27
Oness Lyngdoh <auditoree9@gmail.com>
2024-05-13 14:40:39 +0530

    refactored to use BottomTab.Bar from rn paper

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
 import AppBottomTab from './src/navigation/AppBottomTab';
+import { PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppBottomTab />
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <AppBottomTab />
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
--- src/navigation/AppBottomTab.tsx
+++ src/navigation/AppBottomTab.tsx
@@ -1,15 +1,58 @@
+import { CommonActions } from '@react-navigation/native';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import DashboardScreen from "../screens/DashboardScreen";
 import ScheduleScreen from "../screens/ScheduleScreen";
 import ApplicationsScreen from "../screens/ApplicationsScreen";
 import MoreScreen from "../screens/MoreScreen";
 import Feather from '@expo/vector-icons/Feather'
+import { BottomNavigation } from "react-native-paper";
 
 const Tab = createBottomTabNavigator();
 
 const AppBottomTab = () => {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            tabBar={({ navigation, state, descriptors, insets }) => (
+                <BottomNavigation.Bar
+                    navigationState={state}
+                    safeAreaInsets={insets}
+                    onTabPress={({ route, preventDefault }) => {
+                        const event = navigation.emit({
+                            type: 'tabPress',
+                            target: route.key,
+                            canPreventDefault: true,
+                        });
+
+                        if (event.defaultPrevented) {
+                            preventDefault();
+                        } else {
+                            navigation.dispatch({
+                                ...CommonActions.navigate(route.name, route.params),
+                                target: state.key,
+                            });
+                        }
+                    }}
+                    renderIcon={({ route, focused, color }) => {
+                        const { options } = descriptors[route.key];
+                        if (options.tabBarIcon) {
+                            return options.tabBarIcon({ focused, color, size: 24 });
+                        }
+
+                        return null;
+                    }}
+                    getLabelText={({ route }) => {
+                        const { options } = descriptors[route.key];
+                        const label =
+                            options.tabBarLabel !== undefined
+                                ? options.tabBarLabel
+                                : options.title !== undefined
+                                    ? options.title
+                                    : route.name;
+
+                        return label;
+                    }}
+                />
+            )}>
             <Tab.Screen name="Dashboard" component={DashboardScreen} options={{
                 tabBarIcon: ({ color, size }) => <Feather name="compass" color={color} size={size} />
             }} />
