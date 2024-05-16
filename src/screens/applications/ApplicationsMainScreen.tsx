66df89999a14d08968e63d525467f1103a56beaf
on3ss <auditoree9@gmail.com>
2024-05-16 23:25:12 +0530

    added FAB to applications screen

--- src/screens/applications/ApplicationsMainScreen.tsx
+++ src/screens/applications/ApplicationsMainScreen.tsx
@@ -1,13 +1,25 @@
 import { StatusBar } from "expo-status-bar"
-import { View } from "react-native"
-import { Text } from "react-native-paper"
+import { StyleSheet, View } from "react-native"
+import { FAB, Text } from "react-native-paper"
 
 const ApplicationsHomeScreen = () => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Applications Page</Text>
+            <FAB icon="plus" style={styles.fab} onPress={() => console.log('Pressed')} ></FAB>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexGrow: 1,
+    },
+    fab: {
+        position: 'absolute',
+        right: 16,
+        bottom: 16,
+    },
+})
 
 export default ApplicationsHomeScreen
