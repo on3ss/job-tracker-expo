c371945e38d32c2af166f6fff02138d02e2551e8
on3ss <auditoree9@gmail.com>
2024-05-16 23:01:28 +0530

    reverted

--- App.tsx
+++ App.tsx
@@ -1,33 +1,36 @@
-import React, { useMemo, useState } from 'react';
-import { StatusBar } from "expo-status-bar";
 import { NavigationContainer, DefaultTheme as NavigationDefaultTheme, DarkTheme as NavigationDarkTheme } from '@react-navigation/native';
-import { PaperProvider, adaptNavigationTheme, MD3DarkTheme, MD3LightTheme } from 'react-native-paper';
-import merge from 'deepmerge';
-import { PreferencesContext } from './src/providers/PreferencesContext';
 import AppBottomTab from './src/navigation/AppBottomTab';
+import { PaperProvider, adaptNavigationTheme, MD3DarkTheme, MD3LightTheme } from 'react-native-paper';
+import merge from 'deepmerge'
+import { useCallback, useMemo, useState } from 'react';
+import { PreferencesContext } from './src/providers/PreferencesContext';
+import { StatusBar } from "expo-status-bar"
 
-const { LightTheme, DarkTheme } = adaptNavigationTheme({ reactNavigationDark: NavigationDefaultTheme, reactNavigationLight: NavigationDarkTheme });
+const { LightTheme, DarkTheme } = adaptNavigationTheme({ reactNavigationDark: NavigationDefaultTheme, reactNavigationLight: NavigationDarkTheme })
 
-const CombinedDefaultTheme = merge(MD3LightTheme, LightTheme);
-const CombinedDarkTheme = merge(MD3DarkTheme, DarkTheme);
+const CombinedDefaultTheme = merge(MD3LightTheme, LightTheme)
+const CombinedDarkTheme = merge(MD3DarkTheme, DarkTheme)
 
 export default function App() {
-  const [isThemeDark, setIsThemeDark] = useState(false);
+  const [isThemeDark, setIsThemeDark] = useState(false)
+  let theme = isThemeDark ? CombinedDarkTheme : CombinedDefaultTheme
 
-  const toggleTheme = () => setIsThemeDark(!isThemeDark);
+  const toggleTheme = useCallback(() => {
+    return setIsThemeDark(!isThemeDark)
+  }, [isThemeDark])
 
   const preferences = useMemo(
     () => ({
       toggleTheme,
       isThemeDark
     }),
-    [isThemeDark]
-  );
+    [toggleTheme, isThemeDark]
+  )
 
   return (
     <PreferencesContext.Provider value={preferences}>
-      <PaperProvider theme={isThemeDark ? CombinedDarkTheme : CombinedDefaultTheme}>
-        <NavigationContainer theme={isThemeDark ? CombinedDarkTheme : CombinedDefaultTheme}>
+      <PaperProvider theme={theme}>
+        <NavigationContainer theme={theme}>
           <StatusBar style={isThemeDark ? 'light' : 'dark'} />
           <AppBottomTab />
         </NavigationContainer>
