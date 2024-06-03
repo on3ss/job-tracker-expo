e5c9f81e8ddb15873e5d48f72185a020912721c7
Oness Lyngdoh <auditoree9@gmail.com>
2024-06-03 12:39:08 +0530

    prettier

--- src/screens/applications/detail/ApplicationDetailScreen.tsx
+++ src/screens/applications/detail/ApplicationDetailScreen.tsx
@@ -2,131 +2,131 @@ import { Octicons } from "@expo/vector-icons";
 import React from "react";
 import { ScrollView, SectionList, View } from "react-native";
 import {
-    Button,
-    Chip,
-    Divider,
-    IconButton,
-    Surface,
-    Text,
-    useTheme,
+  Button,
+  Chip,
+  Divider,
+  IconButton,
+  Surface,
+  Text,
+  useTheme,
 } from "react-native-paper";
 import useStyles from "./styles";
 import { MOCK_STAGES, MOCK_NEXT_STAGE, Stage } from "./mock-data";
 
 const ApplicationDetailScreen: React.FC = () => {
-    const theme = useTheme();
-    const styles = useStyles();
-    return (
-        <ScrollView>
-            <View style={styles.container}>
-                <Surface style={styles.titleCard} mode="flat">
-                    <Text variant="titleLarge" style={styles.titleText}>
-                        Lower Division Assistant
-                    </Text>
-                    <View style={styles.subTitleWrapper}>
-                        <Octicons
-                            name="organization"
-                            size={16}
-                            color={theme.colors.onSurface}
-                        />
-                        <Text variant="bodyMedium" style={styles.organisationText}>
-                            Department of Accounts & Treasuries
-                        </Text>
-                    </View>
-                    <View style={styles.subTitleWrapper}>
-                        <Octicons name="browser" size={16} color={theme.colors.onSurface} />
-                        <Text variant="bodyMedium" style={styles.organisationText}>
-                            MPSC
-                        </Text>
-                    </View>
-                </Surface>
-                <View style={styles.nextStepContainer}>
-                    <Text
-                        variant="titleMedium"
-                        style={{ textTransform: "uppercase", fontWeight: "bold" }}
-                    >
-                        Next Stage
-                    </Text>
-                    <StepListItem item={MOCK_NEXT_STAGE} />
-                </View>
-                <View style={styles.upcomingStepsContainer}>
-                    <View
-                        style={{
-                            flexDirection: "row",
-                            justifyContent: "space-between",
-                            alignItems: "center",
-                        }}
-                    >
-                        <Text
-                            variant="titleMedium"
-                            style={{ textTransform: "uppercase", fontWeight: "bold" }}
-                        >
-                            Stages
-                        </Text>
-                        <IconButton
-                            icon="plus"
-                            mode="contained"
-                            iconColor={theme.colors.primary}
-                            size={18}
-                            onPress={() => console.log("New Stage")}
-                        />
-                    </View>
-                    <SectionList
-                        sections={MOCK_STAGES}
-                        keyExtractor={(item) => item.id.toString()}
-                        renderItem={({ item }) => <StepListItem item={item} />}
-                        renderSectionHeader={({ section: { title } }) => (
-                            <Text variant="labelLarge" style={{ marginTop: 12 }}>
-                                {title.format("ddd, MMM D, YYYY")}
-                            </Text>
-                        )}
-                        scrollEnabled={false}
-                    />
-                </View>
-            </View>
-        </ScrollView>
-    );
+  const theme = useTheme();
+  const styles = useStyles();
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Surface style={styles.titleCard} mode="flat">
+          <Text variant="titleLarge" style={styles.titleText}>
+            Lower Division Assistant
+          </Text>
+          <View style={styles.subTitleWrapper}>
+            <Octicons
+              name="organization"
+              size={16}
+              color={theme.colors.onSurface}
+            />
+            <Text variant="bodyMedium" style={styles.organisationText}>
+              Department of Accounts & Treasuries
+            </Text>
+          </View>
+          <View style={styles.subTitleWrapper}>
+            <Octicons name="browser" size={16} color={theme.colors.onSurface} />
+            <Text variant="bodyMedium" style={styles.organisationText}>
+              MPSC
+            </Text>
+          </View>
+        </Surface>
+        <View style={styles.nextStepContainer}>
+          <Text
+            variant="titleMedium"
+            style={{ textTransform: "uppercase", fontWeight: "bold" }}
+          >
+            Next Stage
+          </Text>
+          <StepListItem item={MOCK_NEXT_STAGE} />
+        </View>
+        <View style={styles.upcomingStepsContainer}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Text
+              variant="titleMedium"
+              style={{ textTransform: "uppercase", fontWeight: "bold" }}
+            >
+              Stages
+            </Text>
+            <IconButton
+              icon="plus"
+              mode="contained"
+              iconColor={theme.colors.primary}
+              size={18}
+              onPress={() => console.log("New Stage")}
+            />
+          </View>
+          <SectionList
+            sections={MOCK_STAGES}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => <StepListItem item={item} />}
+            renderSectionHeader={({ section: { title } }) => (
+              <Text variant="labelLarge" style={{ marginTop: 12 }}>
+                {title.format("ddd, MMM D, YYYY")}
+              </Text>
+            )}
+            scrollEnabled={false}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
 };
 
 const StepListItem: React.FC<{ item: Stage }> = ({ item }) => {
-    const styles = useStyles();
+  const styles = useStyles();
 
-    return (
-        <Surface style={styles.stepContainer} mode="flat">
-            <View style={styles.stepHeader}>
-                <Text variant="titleMedium" style={styles.stepTitle}>
-                    {item.name}
-                </Text>
-                <Text variant="bodyMedium" style={styles.stepVenue}>
-                    {item.venue}
-                </Text>
-            </View>
-            <ScrollView style={styles.subjectsWrapper} horizontal>
-                {item.subjects.map((subject, index) => (
-                    <Chip
-                        key={subject.id}
-                        compact
-                        textStyle={styles.chipText}
-                        style={[
-                            styles.chip,
-                            index === item.subjects.length - 1 && styles.lastChip,
-                        ]}
-                    >
-                        {subject.name}
-                    </Chip>
-                ))}
-            </ScrollView>
-            <Divider />
-            <View style={styles.stepFooter}>
-                <Text variant="bodyMedium" style={styles.stepTime}>
-                    {item.datetime.format("h:mmA")}
-                </Text>
-                <Text variant="bodyMedium" style={styles.stepDate}>
-                    {item.datetime.format("ddd, MMM D, YYYY")}
-                </Text>
-            </View>
-        </Surface>
-    );
+  return (
+    <Surface style={styles.stepContainer} mode="flat">
+      <View style={styles.stepHeader}>
+        <Text variant="titleMedium" style={styles.stepTitle}>
+          {item.name}
+        </Text>
+        <Text variant="bodyMedium" style={styles.stepVenue}>
+          {item.venue}
+        </Text>
+      </View>
+      <ScrollView style={styles.subjectsWrapper} horizontal>
+        {item.subjects.map((subject, index) => (
+          <Chip
+            key={subject.id}
+            compact
+            textStyle={styles.chipText}
+            style={[
+              styles.chip,
+              index === item.subjects.length - 1 && styles.lastChip,
+            ]}
+          >
+            {subject.name}
+          </Chip>
+        ))}
+      </ScrollView>
+      <Divider />
+      <View style={styles.stepFooter}>
+        <Text variant="bodyMedium" style={styles.stepTime}>
+          {item.datetime.format("h:mmA")}
+        </Text>
+        <Text variant="bodyMedium" style={styles.stepDate}>
+          {item.datetime.format("ddd, MMM D, YYYY")}
+        </Text>
+      </View>
+    </Surface>
+  );
 };
 
 export default ApplicationDetailScreen;
--- src/screens/applications/detail/mock-data.ts
+++ src/screens/applications/detail/mock-data.ts
@@ -1,100 +1,100 @@
 import dayjs from "dayjs";
 
 const MOCK_STAGES: GroupedStage[] = [
-  {
-    title: dayjs("2024-06-01T09:00:00"),
-    data: [
-      {
-        id: 1,
-        name: "Introduction to Programming",
-        description:
-          "An introductory course on programming concepts using Python.",
-        datetime: dayjs("2024-06-01T09:00:00"),
-        venue: "Room 101, Computer Science Building",
-        subjects: [
-          { id: 1, name: "Variables and Data Types" },
-          { id: 2, name: "Control Structures" },
+    {
+        title: dayjs("2024-06-01T09:00:00"),
+        data: [
+            {
+                id: 1,
+                name: "Introduction to Programming",
+                description:
+                    "An introductory course on programming concepts using Python.",
+                datetime: dayjs("2024-06-01T09:00:00"),
+                venue: "Room 101, Computer Science Building",
+                subjects: [
+                    { id: 1, name: "Variables and Data Types" },
+                    { id: 2, name: "Control Structures" },
+                ],
+                isDone: false,
+            },
+            {
+                id: 2,
+                name: "Advanced Algorithms",
+                description:
+                    "A deep dive into advanced algorithmic techniques and data structures.",
+                datetime: dayjs("2024-06-01T10:00:00"),
+                venue: "Room 202, Computer Science Building",
+                subjects: [
+                    { id: 3, name: "Graph Algorithms" },
+                    { id: 4, name: "Dynamic Programming" },
+                ],
+                isDone: false,
+            },
         ],
-        isDone: false,
-      },
-      {
-        id: 2,
-        name: "Advanced Algorithms",
-        description:
-          "A deep dive into advanced algorithmic techniques and data structures.",
-        datetime: dayjs("2024-06-01T10:00:00"),
-        venue: "Room 202, Computer Science Building",
-        subjects: [
-          { id: 3, name: "Graph Algorithms" },
-          { id: 4, name: "Dynamic Programming" },
+    },
+    {
+        title: dayjs("2024-06-15T11:00:00"),
+        data: [
+            {
+                id: 3,
+                name: "Database Systems",
+                description: "An overview of database design, management, and SQL.",
+                datetime: dayjs("2024-06-15T11:00:00"),
+                venue: "Room 303, Computer Science Building",
+                subjects: [
+                    { id: 5, name: "Relational Databases" },
+                    { id: 6, name: "SQL Queries" },
+                ],
+                isDone: false,
+            },
+            {
+                id: 4,
+                name: "Web Development",
+                description:
+                    "Learning the basics of web development including HTML, CSS, and JavaScript.",
+                datetime: dayjs("2024-06-22T12:00:00"),
+                venue: "Room 404, Computer Science Building",
+                subjects: [
+                    { id: 7, name: "HTML and CSS" },
+                    { id: 8, name: "JavaScript Basics" },
+                ],
+                isDone: false,
+            },
         ],
-        isDone: false,
-      },
-    ],
-  },
-  {
-    title: dayjs("2024-06-15T11:00:00"),
-    data: [
-      {
-        id: 3,
-        name: "Database Systems",
-        description: "An overview of database design, management, and SQL.",
-        datetime: dayjs("2024-06-15T11:00:00"),
-        venue: "Room 303, Computer Science Building",
-        subjects: [
-          { id: 5, name: "Relational Databases" },
-          { id: 6, name: "SQL Queries" },
-        ],
-        isDone: false,
-      },
-      {
-        id: 4,
-        name: "Web Development",
-        description:
-          "Learning the basics of web development including HTML, CSS, and JavaScript.",
-        datetime: dayjs("2024-06-22T12:00:00"),
-        venue: "Room 404, Computer Science Building",
-        subjects: [
-          { id: 7, name: "HTML and CSS" },
-          { id: 8, name: "JavaScript Basics" },
-        ],
-        isDone: false,
-      },
-    ],
-  },
+    },
 ];
 
 const MOCK_NEXT_STAGE: Stage = {
-  id: 1,
-  name: "Introduction to Programming",
-  description: "An introductory course on programming concepts using Python.",
-  datetime: dayjs("2024-06-01T09:00:00"),
-  venue: "Room 101, Computer Science Building",
-  subjects: [
-    { id: 1, name: "Variables and Data Types" },
-    { id: 2, name: "Control Structures" },
-  ],
-  isDone: false,
+    id: 1,
+    name: "Introduction to Programming",
+    description: "An introductory course on programming concepts using Python.",
+    datetime: dayjs("2024-06-01T09:00:00"),
+    venue: "Room 101, Computer Science Building",
+    subjects: [
+        { id: 1, name: "Variables and Data Types" },
+        { id: 2, name: "Control Structures" },
+    ],
+    isDone: false,
 };
 
 type GroupedStage = {
-  title: dayjs.Dayjs;
-  data: Stage[];
+    title: dayjs.Dayjs;
+    data: Stage[];
 };
 
 type Stage = {
-  id: number;
-  name: string;
-  description: string;
-  datetime: dayjs.Dayjs;
-  venue: string;
-  subjects: Subject[];
-  isDone: boolean;
+    id: number;
+    name: string;
+    description: string;
+    datetime: dayjs.Dayjs;
+    venue: string;
+    subjects: Subject[];
+    isDone: boolean;
 };
 
 type Subject = {
-  id: number;
-  name: string;
+    id: number;
+    name: string;
 };
 
 export { MOCK_STAGES, MOCK_NEXT_STAGE, GroupedStage, Stage, Subject };
